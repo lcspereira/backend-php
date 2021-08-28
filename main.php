f417b6a15502bf8ef1bd3e5c2c62ea3d34821f8a
Lucas Pereira <lucas.pereira6c@gmail.com>
2021-08-28 19:44:15 -0300

    [FEAT] Implementação principal

--- main.php
+++ main.php
@@ -2,8 +2,53 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-use Moovin\Job\Backend;
+use Moovin\Job\Backend\CheckingAccount;
+use Moovin\Job\Backend\SavingAccount;
 
-$exemplo = new Backend\Exemplo;
+$cc = new CheckingAccount(1000);
+$cc2 = new CheckingAccount(400);
+$poupanca = new SavingAccount(2000);
+$poupanca2 = new SavingAccount(4000);
 
-echo $exemplo->exemplo();
+echo $cc . PHP_EOL;
+echo $cc2 . PHP_EOL;
+echo $poupanca . PHP_EOL;
+echo $poupanca2 . PHP_EOL;
+echo PHP_EOL;
+
+$cc->withdraw(100);
+echo $cc . PHP_EOL;
+echo PHP_EOL;
+
+$cc2->deposit(200);
+echo $cc2 . PHP_EOL;
+echo PHP_EOL;
+
+$cc2->transfer($cc, 100);
+echo $cc . PHP_EOL;
+echo $cc2 . PHP_EOL;
+echo PHP_EOL;
+
+$poupanca->withdraw(100);
+echo $poupanca . PHP_EOL;
+echo PHP_EOL;
+
+$poupanca2->deposit(200);
+echo $poupanca2 . PHP_EOL;
+echo PHP_EOL;
+
+$poupanca2->transfer($poupanca, 100);
+echo $poupanca . PHP_EOL;
+echo $poupanca2 . PHP_EOL;
+echo PHP_EOL;
+
+$cc->transfer($poupanca2, 200);
+$poupanca->transfer($cc2, 400);
+
+echo $cc . PHP_EOL;
+echo $cc2 . PHP_EOL;
+echo $poupanca . PHP_EOL;
+echo $poupanca2 . PHP_EOL;
+echo PHP_EOL;
+
+echo "Fim do programa" . PHP_EOL;
